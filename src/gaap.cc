85548a11991e557d0dbbaba5777ed80215dc46a6
Mark Carter <alt.mcarter@gmail.com>
2016-06-19 11:17:19 +0100

    Improved gaap

--- src/gaap.cc
+++ src/gaap.cc
@@ -4,10 +4,12 @@
 
 #include <map>
 #include <iostream>
+#include <fstream>
 #include <locale.h>
 //#include "autosprintf.h"
 #include <string.h>
 
+#include "common.hpp"
 #include "parse.hpp"
 #include "reusable.hpp"
 #include "gaap.hpp"
@@ -16,9 +18,9 @@
 
 using namespace std;
 
-void ul1(ostream& ofs) { ofs << nchars(' ', 11) << nchars('-', 10) << endl;};
+void ul1(ostream* ofs) { (*ofs) << nchars(' ', 11) << nchars('-', 10) << endl;};
 
-void emit(ostream& ofs, const string& title, pennies_t value)
+void emit(ostream* ofs, const string& title, pennies_t value)
 {
 	char sgn = ' ';
 	if (value<0) { sgn = '-' ; value = -value;}
@@ -28,87 +30,144 @@ void emit(ostream& ofs, const string& title, pennies_t value)
 	snprintf(buf, sizeof(buf), "%10.10s %'10.2f%c", title.c_str() , bal, sgn);
 	//ofs << gnu::autosprintf("%10.10s %'10.2f%c", title.c_str() , bal, sgn);
 	//ofs << endl;
-	ofs << buf << endl;
+	(*ofs) << buf << endl;
 
 
 }
 
-void subtotal(ostream& ofs, const string& title, pennies_t value)
+// both set by gaap_main()
+ostream* m_ofs = nullptr;
+nacc_ts m_naccs;
+
+pennies_t get_bal(string key)
+{ 
+	pennies_t bal = 0;
+	try {
+		bal = m_naccs.at(key).bal;
+	} catch (const std::out_of_range& ex) {
+		bal = 0;
+		//cerr << "gaap_main() couldn't look up key " << key << " in naccs" << endl;
+		//throw ex;
+	}	
+	return bal;
+}	
+
+struct Lie { string desc; pennies_t amount = 0; bool oline = false ; bool uline = false;}; // line entry
+void print_lie(const Lie& lie) 
+{ 
+	if(lie.oline) ul1(m_ofs);
+	emit(m_ofs, lie.desc, lie.amount);
+	if(lie.uline) ul1(m_ofs);
+}
+
+class section {
+	public:
+		section(string desc) {
+			total_lie.desc = desc;
+		       	total_lie.oline = true;
+			total_lie.uline = true;
+		}
+		
+		//string desc;
+		//int amount;
+		//pennies_t total = 0;
+		//static ofstream& ofs;
+		//static int foo;
+		
+		section add(string desc) {
+			pennies_t amount = get_bal(desc);
+			struct Lie lie = {desc, amount};
+			//lies.push_back(lie);
+			//total_lie.amount += amount;
+			add(lie);
+			return *this;
+		}
+
+		section add(section s) {
+			struct Lie lie {s.total_lie.desc, s.total_lie.amount};
+			add(lie);
+			return *this;
+		}
+
+		section add(Lie lie) {
+			lies.push_back(lie);
+			total_lie.amount += lie.amount;
+			return *this;
+		}
+
+		section adds(vector<string> descs) {
+			for(auto& d:descs) add(d);
+			return *this;
+		}
+
+		section running(string desc) {
+			struct Lie lie = total_lie;
+			lie.desc = desc;
+			lie.uline = false;
+			lies.push_back(lie);
+			return *this;
+		}
+
+		void print() {
+		       	for(auto& lie:lies)  print_lie(lie);
+			//ul1(m_ofs);
+			print_lie( total_lie);
+			(*m_ofs) << endl;
+		}
+	private:
+		struct Lie total_lie; 
+		vector<Lie> lies;
+};
+
+void subtotal(ostream* ofs, const string& title, pennies_t value)
 { 
 	ul1(ofs); emit(ofs, title, value); ul1(ofs);
-	ofs << endl;
+	(*ofs) << endl;
 
 }
 
 void gaap_main(const nacc_ts& the_naccs)
 {
+	m_naccs = the_naccs;
+
 	auto get_bal = [the_naccs](auto key){ 
 		pennies_t bal = 0;
 		try {
 			bal = the_naccs.at(key).bal;
 		} catch (const std::out_of_range& ex) {
 			bal = 0;
-			//cerr << "gaap_main() couldn't look up key " << key << " in naccs" << endl;
-			//throw ex;
 		}	
 		return bal;
 	};	
 
 
-	pennies_t inter = get_bal("int");
-	pennies_t div   = get_bal("div");
-	pennies_t wag   = get_bal("wag");
-	pennies_t inc = inter + div + wag;
+	string fname;
+	s3("gaap.rep", fname);
+	ofstream ofs;
+	ofs.open(fname.c_str(), ofstream::out);
+	m_ofs = &ofs;
+	section inco = section("Income").adds({"div", "int", "wag"});
+	section exps = section("Expenses").adds( {"amz", "car", "chr", "cmp", "hol", "isp", "msc", "mum"});
+	section mygains = section("Folio Gain").adds( {"hal_g", "hl_g", "igg_g", "tdi_g", "tdn_g"});
+	section balcd = section("Bal c/d").add(inco).add(exps).running("Ord profit")
+		.add("tax").add(mygains).add("ut_g").running("Net Profit")
+		.add("opn");
+	section folio = section("Folio")
+		.adds( {"hal_c", "hl_c", "igg_c", "tdi_c", "tdn_c"})
+		.running("My shares")
+		.add("ut_c");
 
-	pennies_t amz = get_bal("amz");
-	pennies_t car = get_bal("car");
-	pennies_t chr = get_bal("chr");
-	pennies_t cmp = get_bal("cmp");
-	pennies_t hol = get_bal("hol");
-	pennies_t isp = get_bal("isp");
-	pennies_t msc = get_bal("msc");
-	pennies_t mum = get_bal("mum");
-	pennies_t exp = amz + car + chr + hol +isp + msc + mum;
+	section nass = section("Net Assets")
+		.adds( {"hal", "hl", "igg", "ut", "rbs", "rbd", "sus", "tdi", "tdn", "tds", "vis"})
+		.running("Cash equiv")
+		.add(folio)
+		.add("msa");
 
-	pennies_t xinc = inc + exp;
-
-
-	auto& ofs = cout;
-
-	emit(ofs, "Income", inc);
-	emit(ofs, "Expenses", exp);
-	ul1(ofs);
-	emit(ofs, "Excess Income", xinc);
-	pennies_t  tax = get_bal("tax");
-	emit(ofs, "Taxes", tax);
-	pennies_t ninc = xinc + tax;
-	subtotal(ofs, "Net Income", ninc);
-
-
-	ofs <<endl;
-
-	emit(ofs, "Interest", inter);
-	emit(ofs, "Dividends", div);
-	emit(ofs, "Wages", wag);
-	subtotal(ofs, "Income", inc);
-
-	emit(ofs, "Amazon", amz);
-	emit(ofs, "Car", car);
-	emit(ofs, "Charity", chr);
-	emit(ofs, "Computing", cmp);
-	emit(ofs, "Holidays", hol);
-	emit(ofs, "ISP", isp);
-	emit(ofs, "Misc", msc);
-	emit(ofs, "Mum", mum);
-	subtotal(ofs, "Expenses", exp);
-	emit(ofs, "Taxes", tax);
-
-	emit(ofs, "Amazon", amz);
-	emit(ofs, "Amazon", amz);
-	emit(ofs, "Amazon", amz);
-	emit(ofs, "Amazon", amz);
-	emit(ofs, "Amazon", amz);
-
-	
-	
+	balcd.print();
+	nass.print();
+	inco.print();
+	exps.print();
+	mygains.print();
+	folio.print();
+	ofs.close();
 }
