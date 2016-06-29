f1c02504866119f9c94ee12b861fc18d1aa573ed
Mark Carter <alt.mcarter@gmail.com>
2016-06-29 18:09:50 +0100

    Enhanced inputs.cc so that it accepts more types
    Yahoos are the only ones outstanding.

--- src/inputs.cc
+++ src/inputs.cc
@@ -42,10 +42,30 @@ etran_t mketran(const strings& fields)
 	e.ticker = fields[3];
 	e.qty= stod(fields[5]);
 	e.cost= stod(fields[6]);
+	e.regular = true;
 	//e.ticker= fields[3];
 	return e;
 }
 
+etran_t mkleak_1(const strings& fields)
+{
+	// TODO LEAK-1 should avaoid making a stend, which I think it does automatically
+	etran_t e = mketran(fields);
+	e.buy = false;;
+	e.regular = false;
+	return e;
+}
+
+ntran_t mkntran(const strings& fields)
+{
+	ntran_t n;
+	n.dstamp=fields[1];
+	n.dr=fields[2];
+	n.cr=fields[3];
+	n.amount=enpennies(fields[6]);
+	n.desc=fields[9];
+	return n;
+}
 void TODO() { cout << "TODO in inputs.cc\n" ; }
 
 void insert_LVL03(inputs_t& inputs, const strings& fields)
@@ -54,9 +74,9 @@ void insert_LVL03(inputs_t& inputs, const strings& fields)
 	if(subtype == "ETRAN-1") { 
 		inputs.etrans.push_back(mketran(fields));
 	} else if (subtype == "LEAK-1") {
-		TODO();
+		inputs.etrans.push_back(mkleak_1(fields));
 	} else if (subtype == "NTRAN-1") {
-		TODO();
+		inputs.ntrans.push_back(mkntran(fields));
 	} else {
 		cerr << "inputs.cc:read_inputs()/LVL03 couldn't understand type ";
 		cerr << subtype << ". Fatal exit." << endl;
@@ -64,6 +84,12 @@ void insert_LVL03(inputs_t& inputs, const strings& fields)
 	}
 }
 
+
+void set_period(inputs_t& inputs, const strings& fields)
+{
+	inputs.p.start_date = fields[2];
+	inputs.p.end_date = fields[3];
+}
 
 inputs_t read_inputs()
 {
@@ -78,27 +104,28 @@ inputs_t read_inputs()
 		string level_str = cmd.substr(3, 2);
 		int level =stoi(level_str);
 		switch(level) {
-			case 0:
+			case 0: //LVL00
 				// these are just notes, so we can ignore them
 				break;
-			case 1:
+			case 1: // LVL01
 				insert_nacc(inputs.naccs, row);
 				break;
-			case 2:
+			case 2: // LVL02
 				insert_comm(inputs.comms, row);
 				break;
-			case 3:
+			case 3: // LVL03
 				insert_LVL03(inputs, row);
 				break;
-			case 4:
+			case 4: // LVL04
+				set_period(inputs, row);
 				break;
-			case 5:
+			case 5: // LVL05
+				TODO();
 				break;
 			default:
 				cerr << "Unhandled level number " << level << " in inputs.cc/read_inputs()\n";
 				exit(EXIT_FAILURE);
 		}
-		// TODO NOW
 	}
 	return inputs;
 
