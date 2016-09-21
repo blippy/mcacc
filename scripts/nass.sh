a115b64231ce34dbf5a6ad6f07d5fc89417ca9ef
Mark Carter <alt.mcarter@gmail.com>
2016-09-21 12:46:46 +0100

    Improvements to cgt

--- scripts/nass.sh
+++ scripts/nass.sh
@@ -2,11 +2,14 @@
 
 source mcacc-env.sh
 
-dtstamp=`date --rfc-3339=seconds`
-nass=`grep "Net Assets" $s3/gaap-0.rep`
-line="$dtstamp $nass"
-echo $line >> $s3/nass-0.rep
-tac <$s3/nass-0.rep >$s3/nass-1.rep
+if false; then
+	# traditional way which we should probably ignore from 21-Sep-2016
+	dtstamp=`date --rfc-3339=seconds`
+	nass=`grep "Net Assets" $s3/gaap-0.rep`
+	line="$dtstamp $nass"
+	echo $line >> $s3/nass-0.rep
+	tac <$s3/nass-0.rep >$s3/nass-1.rep
+fi
 
 # alternate way
 (cd $w/gaap && grep "Net Assets" *.txt) | sed -e 's/.txt:/   /' | sort -r >$s3/nass-2.rep
--- src/cgt.cc
+++ src/cgt.cc
@@ -1,29 +1,61 @@
 #include "cgt.hpp"
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <ostream>
+#include <cmath>
 #include <set>
 #include <string>
 
-using std::cout;
+#include "common.hpp"
+#include "reusable.hpp"
+
+//using std::cout;
 using std::endl;
+using std::ofstream;
 using std::string;
+
+string mkrow(etran_t& e)
+{
+	string bs = e.buy? "B" : "S"; // TODO LOW could be abstracted into etran..cc
+
+	string y = e.dstamp.substr(0, 4);
+	string m = e.dstamp.substr(5, 2);
+	string d = e.dstamp.substr(8, 2);
+	string dstamp = d + "-" + m + "-" + y;
+
+	string share_str = format_num(fabs(e.qty), 4);
+	
+	string price_str = format_num(fabs(e.cost/e.qty/100), 5);
+
+	return  intercalate("\t", {bs, dstamp, e.ticker, share_str, price_str, "0.00", "0.00"});
+	//return result;
+}
+
+//bool match(etran_t &e)
+//{
+//	return true;
+//}
 
 void cgt(const etran_ts& es, const period &per)
 {
-	cout << "mcacc cgt" << endl;
+	string fname;
+	s3("cgt-1.rep", fname);
+	ofstream sout;
+	sout.open(fname);
 
-	//string start_date, end_date;
-	//get_period(start_date, end_date);
-	
 	std::set<string> tickers;
-	for(auto e: es) {
+	for(auto& e: es)
 		if(e.taxable && per.during(e.dstamp)  && ! e.buy) tickers.insert(e.ticker);
-	}
 
-	for(auto & t: tickers) {
-		// NB take care of leak-1 entries, which confuse the issue
-		cout << t << endl; // TODO it's here that we need to do something with the hit tickers
-	}
-	cout << "TODO: cgt - pending something interesting to report" << endl;
+	etran_ts es1;
+	for(auto& e:es)
+		if(tickers.find(e.ticker) != end(tickers))
+			es1.push_back(e);
+
+	for(auto& e:es1) sout << mkrow(e) << endl;
+
+	sout.close();
+
 }
