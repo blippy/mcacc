f1406c674b6be277c12ff6f2d5a772dac97cae8f
Mark Carter <alt.mcarter@gmail.com>
2016-09-22 12:41:53 +0100

    modified:   scrub-clean.sh

--- scrub-clean.sh
+++ scrub-clean.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+(cd scripts; rm -f Makefile Makefile.in)
 rm -rf src/.deps
-rm -f src/Makefile.in src/Makefile
+rm -f src/Makefile.in src/Makefile src/mcacc src/*.o
 rm -f aclocal.m4      configure  install-sh  Makefile.in configure.lineno
 rm -rf autom4te.cache
 rm -f depcomp           missing
