dfc04f9f87fe62ca1db8ba3505fffc20f897418b
Mark Carter <alt.mcarter@gmail.com>
2016-10-29 19:56:24 +0100

    modified:   ../autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 autoreconf -iv
-./configure --prefix=$HOME/.local CXXFLAGS='-ggdb -std=gnu++14'
+#./configure --prefix=$HOME/.local CXXFLAGS='-ggdb -std=gnu++14'
+./configure --prefix=$HOME/.local CXXFLAGS='-ggdb'
